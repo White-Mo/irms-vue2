ec4cd1e565a5254347e5292a2b4a3dc2a83a650f
戈亓 <1994975650@qq.com>
2022-11-07 20:29:50 +0800

    upload bug changed

--- src/views/authority/group.vue
+++ src/views/authority/group.vue
@@ -23,6 +23,34 @@
               <el-button type="primary" size="large" @click="addDialog = true">添加角色</el-button>
             </el-col>
           </el-row>
+          <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+              prop=""
+              label="日期"
+              width="180">
+              <template slot-scope="scope">
+                <i class="el-icon-time"></i>
+                <span style="margin-left: 10px">{{ scope.row.date }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column
+              label="状态"
+              width="180">
+            </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+              </template>
+            </el-table-column>
+            </el-table>
         </div>
       </div>
     </div>
@@ -59,7 +87,24 @@ export default {
       addForm:{
         name: '',
         status:''
-      }
+      },
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
     }
   }
 }
