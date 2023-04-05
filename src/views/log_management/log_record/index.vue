fdbe2be065b311a9153114b3b93e6e9883006fcb
联想 <1335245845@qq.com>
2023-04-05 22:00:32 +0800

    前端日志记录代码

--- src/views/log_management/log_record/index.vue
+++ src/views/log_management/log_record/index.vue
@@ -6,18 +6,33 @@
     <div class="app-container" style="height: 100%">
       <el-row :gutter="20">
         <el-col>
-          <div class="grid-content bg-purple-dark">日志管理</div>
-
+          <div class="grid-content bg-purple-dark">日志管理
+          </div>
         </el-col>
         <el-col :span=8 style="margin-top: 10px">
+          <el-calendar v-model="dateValue" @change="printDate">
+<!--          <el-calendar v-model="dateValue" ref="calendar" @click="handleDateClick" >-->
+          <template
+            slot="dateCell"
+            slot-scope="{date, data}"
+          >
+            <div>
+              <div v-for="(item, key) in activeday" :key="key">
+                <el-badge v-if="data.day == item.dat" is-dot class="item"></el-badge>
+              </div>
+              <div class="spandate">{{ data.day.split('-').slice(2).join('-') }}</div>
+            </div>
+          </template>
 
-          <el-calendar v-model="value" >
           </el-calendar>
+          <p>当前选中日期：{{ activeday }}</p>
+<!--          <button @click="selectDate">选中日期</button>-->
         </el-col >
         <el-col :span="15">
           <div class="block">
+
             <el-timeline>
-              <el-timeline-item timestamp="time" placement="top">
+              <el-timeline-item timestamp="2023/4/6" placement="top">
                 <el-card>
                   <el-table
                     height="70vh"
@@ -44,16 +59,62 @@
                   </el-table>
                 </el-card>
               </el-timeline-item>
-              <el-timeline-item timestamp="2018/4/3" placement="top">
+
+              <el-timeline-item timestamp="2023/4/3" placement="top">
                 <el-card>
-                  <h4>更新 Github 模板</h4>
-                  <p>王小虎 提交于 2018/4/3 20:46</p >
+                  <el-table
+                    height="70vh"
+                    :row-style="{height:'6.26vh'}"
+                    :cell-style="{padding:'0px'}"
+                    v-loading="listLoading"
+                    :disable="true"
+                    :data="tableData"
+                    element-loading-text="Loading"
+                    border
+                    highlight-current-row
+                    stripe
+                  >
+                    <el-table-column align="center" type="index" :index="typeIndex"/>
+                    <el-table-column
+                      v-for="(item,index) in basicvalue"
+                      :key="index"
+                      :label="item.label"
+                      :prop="item.value"
+                      :formatter="item.formatter"
+                      align="center"
+                    >
+                    </el-table-column>
+                  </el-table>
+<!--                  <h4>更新 Github 模板</h4>-->
+<!--                  <p>王小虎 提交于 2018/4/3 20:46</p >-->
                 </el-card>
               </el-timeline-item>
-              <el-timeline-item timestamp="2018/4/2" placement="top">
+
+              <el-timeline-item timestamp="2023/4/2" placement="top">
                 <el-card>
-                  <h4>更新 Github 模板</h4>
-                  <p>王小虎 提交于 2018/4/2 20:46</p >
+                  <el-table
+                    height="70vh"
+                    :row-style="{height:'6.26vh'}"
+                    :cell-style="{padding:'0px'}"
+                    v-loading="listLoading"
+                    :disable="true"
+                    :data="tableData"
+                    element-loading-text="Loading"
+                    border
+                    highlight-current-row
+                    stripe
+                  >
+                    <el-table-column align="center" type="index" :index="typeIndex"/>
+                    <el-table-column
+                      v-for="(item,index) in basicvalue"
+                      :key="index"
+                      :label="item.label"
+                      :prop="item.value"
+                      :formatter="item.formatter"
+                      align="center"
+                    >
+                    </el-table-column>
+                  </el-table>
                 </el-card>
               </el-timeline-item>
             </el-timeline>
@@ -72,16 +133,22 @@ export default {
   name: 'help',
   data() {
     return {
-      tableData:[],
-      listLoading:false,
-      value: new Date(),
-      currentPage:1,
-      limit:10,
-      time:[],
+      tableData: [],
+      listLoading: false,
+      dateValue: new Date(),
+      selectedDate:null,
+      currentPage: 1,
+      limit: 10,
+      time: [],
+      activeday: null,
       basicvalue: [
         {
           value: 'time',
           label: '时间'
+        },
+        {
+          value: 'user',
+          label: '用户'
         },
         {
           value: 'message',
@@ -92,35 +159,62 @@ export default {
   },
   created() {
     this.showData()
+    // this.handleSelect()
+    this.printDate()
+    // this.handleDateClick()
   },
-  methods:{
-    showData(){
-      const params = {
-        start:this.currentPage-1,
-        limit:this.limit
-      }
-      getLogData(params).then(response=>{
-        this.tableData = response.data
-      })
+
+  methods: {
+    printDate(date) {
+      console.log('选中日期:', date);
+      this.activeday = date; // 更新 activeday 的值
     },
-
-
-    //点击下载使用说明文档
-    DownHelpDocument() {
-      // specification.docx文件存储在public文件夹下
-      let a = document.createElement('a')
-      a.href = './static/specification.docx'  //下载链接
-      a.download = '使用说明.docx'  //下载后文件名
-      // a.style.display = 'none'
-      document.body.appendChild(a)
-      a.click() //点击下载
-      a.remove() //下载完成移除元素
-    },
-
-    typeIndex(index){
-      return index+(this.currentPage-1)*this.limit + 1
+    // printDate(date) {
+    //   console.log("所选日期是：", date);
+    // },
+    // handleDateClick(event) {
+    //   const calendar = this.$refs.calendar;
+    //   const selectedDate = calendar.calendarView.activeDate; // 获取当前所选日期
+    //   console.log("所选日期是：", selectedDate);
+    // },
+    // selectDate() {
+    //   // 选中日期
+    //   const calendar = this.$refs.calendar;
+    //   const selectedDate = new Date("2023-04-01"); // 指定选中的日期
+    //   calendar.value = selectedDate; // 设置value属性
+    //   console.log("所选日期是：", selectedDate);
+    // },
+    // handleDateChange(date) {
+    //   console.log('选中日期:', date);
+    // },
+  showData() {
+    const params = {
+      start: this.currentPage - 1,
+      limit: this.limit
     }
+    getLogData(params).then(response => {
+      this.tableData = response.data
+    })
+  },
+
+  //点击下载使用说明文档
+  DownHelpDocument() {
+    // specification.docx文件存储在public文件夹下
+    let a = document.createElement('a')
+    a.href = './static/specification.docx'  //下载链接
+    a.download = '使用说明.docx'  //下载后文件名
+    // a.style.display = 'none'
+    document.body.appendChild(a)
+    a.click() //点击下载
+    a.remove() //下载完成移除元素
+  },
+
+
+
+  typeIndex(index) {
+    return index + (this.currentPage - 1) * this.limit + 1
   }
+  },
 
 
 }
@@ -161,7 +255,9 @@ body,html{
 }
 .box{
   width: 100%;
-
+}
+.is-selected {
+  color: #1989FA;
 }
 
 </style>
