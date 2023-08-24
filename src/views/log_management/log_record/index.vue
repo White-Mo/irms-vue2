3d829570ef55a682e295c916218ac8f53ac154a3
XYY <859942718@qq.com>
2023-08-24 22:28:34 +0800

    8月24日-机房3.0

--- src/views/log_management/log_record/index.vue
+++ src/views/log_management/log_record/index.vue
@@ -290,6 +290,7 @@ export default {
     //选择日志时间进行返回用户以及行为
     handleDateChange(date) {
       // 重置相关变量，以避免显示旧数据
+      this.tableData = []
       this.userData = [];
       this.handlersData = [];
       this.roleData = [];
@@ -381,7 +382,7 @@ export default {
         userName: row.postName,
         time: moment(row.time).format('YYYY-MM-DD')
       }
-      console.log("params",params)
+      console.log("row.postName",row.postName)
       //接口获取前端传取时间参数和相应单位下所有用户操作行为
       getRoleAndCountByCurrentDay(params).then(response => {
         this.tableData = response.data.items
